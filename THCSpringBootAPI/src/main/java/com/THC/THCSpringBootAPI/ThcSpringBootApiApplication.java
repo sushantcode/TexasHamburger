9b0432a3576f8edf6d90fb917f033ed201ccf5e3
sushantcode <sushantgupta@Sushants-MBP.attlocal.net>
2022-01-05 13:06:56 -0600

    project added

---
+++ THCSpringBootAPI/src/main/java/com/THC/THCSpringBootAPI/ThcSpringBootApiApplication.java
@@ -0,0 +1,13 @@
+package com.THC.THCSpringBootAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThcSpringBootApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThcSpringBootApiApplication.class, args);
+	}
+
+}
---
+++ THCSpringBootAPI/src/test/java/com/THC/THCSpringBootAPI/ThcSpringBootApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.THC.THCSpringBootAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThcSpringBootApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
